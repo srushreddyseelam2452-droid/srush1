a03f215d0299ad563618694b3dc7b3fd7a3d7be7
srushreddyseelam2452-droid <srushreddyseelam2452@gmail.com>
2026-04-16 21:20:08 +0530

    Add files via upload

---
+++ personal details.py
@@ -0,0 +1,13 @@
+
+Name=int(input('Enter name'))
+Age=eval(input('enter age:'))
+gender=(input("enter the gender: "))
+Height=eval(input('enter height'))
+print("user Details are as follows: ")
+print("Name: ",Name)
+print('Age: ',Age)
+print('gender: ',gender)
+print('Height ', Height)
+
+
+#if we have input it will give like code chef passage is given then we can 
